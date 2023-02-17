53ebe5957a13d63d942bf157bab72c35a092c834
Pyhajarvi Maaret <maaret.pyhajarvi@vaisala.com>
2023-02-17 21:23:03 +0200

    Remove webdrivermanager and rely on seleniummanager

--- conftest.py
+++ conftest.py
@@ -1,11 +1,9 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver = webdriver.Chrome()
     driver.implicitly_wait(10)
     driver.get("https://todomvc.com/examples/vanillajs/")
     yield driver
