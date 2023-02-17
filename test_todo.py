fb005ae8ba0f2bf5702a1c64155e59caa48ee7b2
Pyhajarvi Maaret <maaret.pyhajarvi@vaisala.com>
2023-02-17 18:15:14 +0200

    Update tests

--- test_todo.py
+++ test_todo.py
@@ -1,14 +1,15 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support import expected_conditions
-from time import sleep
+
 
 def test_add_basic_todo(driver):
     #basic instructions outdated
-    driver.find_element(by=By.CLASS_NAME, value="new-todo").send_keys("Very Important\n")
+    text_field = driver.find_element(by=By.CLASS_NAME, value="new-todo")
+    text_field.send_keys("Very Important")
+    text_field.send_keys(Keys.ENTER)
     assert driver.find_element(By.CSS_SELECTOR, ".todo-list li label").text == "Very Important"
-    #assert driver.find_elements(By.TAG_NAME, "label")[1].text == "Very Important"
-
 
 def test_add_todo(driver):
     #repeat 5 times
@@ -16,7 +17,6 @@ def test_add_todo(driver):
         driver.find_element(By.CLASS_NAME, "new-todo").send_keys(f"Test todo {i}\n")
     assert len(driver.find_elements(By.CSS_SELECTOR, ".toggle")) == 5
     driver.find_elements(By.CSS_SELECTOR, ".toggle")[3].click()
-    sleep(2)
     driver.get_screenshot_as_file('sample_screenshot.png')
     assert len(driver.find_elements(By.CSS_SELECTOR, ".toggle")) == 5
     driver.find_element(By.CSS_SELECTOR, "[href='#/active']").click()
